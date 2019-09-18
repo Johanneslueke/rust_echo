b91c2c8ad7470b6d5e22fa67bda00448212b68ea
Johannes Lüke <lueke.Johannes@googlemail.com>
2019-09-18 15:56:04 +0000

    refactored main and lib

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,6 +1,44 @@
+extern crate clap;
+use clap::{ArgMatches};
 
 
+pub fn escape(input : &str) -> String{
 
-pub fn echo ( text : & str)-> & str {
-    text
+    if ! input.contains("\\") {
+        return String::from(input);
+    }
+
+    let mut escaped = String::from("");
+    for (_index,ch)  in input.char_indices(){
+        if ! (ch == '\\'){
+            escaped.push(ch);
+            continue;
+        }
+        
+    }
+    escaped
+}
+
+pub fn echo (matches : ArgMatches) {
+
+ let input = matches.values_of("INPUT");
+    match input {
+        Some(x) => {
+            let input = &x.fold(String::from(""),
+                               |prev,cur| {
+                                   String::from(prev) + &String::from(cur)
+                                });
+            match matches {
+                _ if matches.is_present("E") => println!("{}",input),
+                _ if matches.is_present("E")
+                  && matches.is_present("n") => print!("{}",input),
+                _ if matches.is_present("n") => print!("{}",escape(input)),
+                _ => println!("{}",escape(input))
+            };
+        }
+        ,
+        None => println!(""),
+    };
+
+  
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,52 @@
 extern crate clap;
-use clap::{Arg, App, SubCommand};
+extern crate echo;
+use clap::{Arg, App};
+
+use echo::echo;
 
 
 
 fn main() {
-    let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+    let matches = App::new(   "Echo"  ) 
+            .arg(
+                Arg::with_name("INPUT")
+                    .multiple(true)
+                    .help("Input stream used for echoing back to the shell")
+            ).get_matches();
+
+
+
+   
+
+   echo(matches);
 }
+
+
+/*
+ let mut cmd : CommandOptions = CommandOptions::new( env::args().collect());
+   let args = &cmd.args[1..];
+
+   for arg in args{
+       if CommandOptions::is_parameter(arg){
+           if arg == "-E"
+            {
+                cmd.disable  = true;
+                cmd.enable = false;
+            }
+            else if arg == "-e" {
+                cmd.disable  = false;
+                cmd.enable = true;
+            }
+            else if arg == "-n"{
+                cmd.ignore_trailing_newline = true;
+            }
+            else
+            {
+                panic!("Unkown argument {}",arg);
+            }
+       }
+       else{
+
+       }
+   }
+*/
